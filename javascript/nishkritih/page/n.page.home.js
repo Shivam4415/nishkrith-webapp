08fddfadb9a026ec4c3ec568745b8a2abba1aa03
Shivam Gupta <shivam441@gmail.com>
2021-04-05 09:45:01 +0530

    home.js updated

--- javascript/nishkritih/page/n.page.home.js
+++ javascript/nishkritih/page/n.page.home.js
@@ -1,27 +1,27 @@
 N.Page.Home= new function(){
- var Ids={
-    MobileRepair:"#mobileRepair"
- };
- var init=function(){
-    var _brands=[{Id:1, 'Name':'Apple','ImageUrl':'https://s3n.cashify.in/cashify/brand/img/xhdpi/1071214e-b44f.jpg'},
-    {'Id':2, 'Name':'Apple','ImageUrl':'https://s3n.cashify.in/cashify/brand/img/xhdpi/1071214e-b44f.jpg'}];
-    N.Page.Brand.init(_brands);
-    // $(Ids.MobileRepair).on('mouseover',function(){
-    //     console.log("hovered");
-    
-    // });
-    // $(Ids.MobileRepair).on('click',function(){
-    //     console.log("clicked");
-    
-    // });
-     
- }
-
-return{
-    init:init
-}
-};
-
-window.onload=function(){
- N.Page.Home.init();
-};
+   var Ids={
+      MobileRepair:"#mobileRepair"
+   };
+   var init=function(){
+      var _brands=[{Id:1, 'Name':'Apple','ImageUrl':'/logo/apple.jpg'},
+      {'Id':2, 'Name':'Apple','ImageUrl':'/logo/realme.jpg'}];
+      N.Page.Brand.init(_brands);
+      // $(Ids.MobileRepair).on('mouseover',function(){
+      //     console.log("hovered");
+      
+      // });
+      // $(Ids.MobileRepair).on('click',function(){
+      //     console.log("clicked");
+      
+      // });
+       
+   }
+  
+  return{
+      init:init
+  }
+  };
+  
+  window.onload=function(){
+   N.Page.Home.init();
+  };
