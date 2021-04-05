e8849302daf871b940dd2aa6f73b4cc751e01c5a
Shivam Gupta <shivam441@gmail.com>
2021-04-05 09:41:38 +0530

    page.util.js  update

--- javascript/nishkritih/page/n.page.util.js
+++ javascript/nishkritih/page/n.page.util.js
@@ -1,21 +1,21 @@
 N.Page.Util=new function(){
     var createCard=function(id,name,imgUrl,type){
         if(type=='Varient'){
-            return '<a class="uk-card-media-left uk-cover-container">'+
-                    '<img src="'+imgUrl+'" uk-cover>'+
+            return '<a class="">'+
+                    '<img src="'+imgUrl+'" uk-cover class="">'+
                     '<canvas width="600" height="400"></canvas>'+
                    '</a>'+
                    '<a class="uk-card-body">'+
-                   '<h3 class="uk-card-title">'+name+'</h3>'+
+                //   '<h3 class="uk-card-title">'+name+'</h3>'+
                    '<p></p>'+
                    '</a>'
         }
-       return '<a class="uk-card uk-card-default" name="'+id+'">'+
-            '<div class="uk-card-media-top">'+
-                '<img src="'+imgUrl+'">'+
+       return '<a class="uk-padding-remove " name="'+id+'">'+
+            '<div class="uk-card uk-card-default uk-card-body uk-margin-left uk-card-small uk-width-small@s">'+
+                '<img src="'+imgUrl+'" class="">'+
             '</div>'+
-            '<div class="uk-card-body">'+
-                '<h3 class="uk-card-title">'+name+'</h3>'+
+            '<div class="uk-card-title">'+
+                // '<h3 class="uk-card-title">'+name+'</h3>'+
             '</div>'+
         '</a>';
     };
