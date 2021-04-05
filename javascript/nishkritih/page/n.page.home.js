3bf6baad1992ce5791db20495ca56b91f0b6836c
Shivam Gupta <shivam441@gmail.com>
2021-04-05 08:13:24 +0530

    add new ui

--- javascript/nishkritih/page/n.page.home.js
+++ javascript/nishkritih/page/n.page.home.js
@@ -3,8 +3,8 @@ N.Page.Home= new function(){
     MobileRepair:"#mobileRepair"
  };
  var init=function(){
-    var _brands=[{Id:1, 'Name':'Apple','ImageUrl':'https://s3n.cashify.in/cashify/brand/img/xhdpi/1071214e-b44f.jpg'},
-    {'Id':2, 'Name':'Apple','ImageUrl':'https://s3n.cashify.in/cashify/brand/img/xhdpi/1071214e-b44f.jpg'}];
+    var _brands=[{Id:1, 'Name':'Apple','ImageUrl':'/logo/apple.jpg'},
+    {'Id':2, 'Name':'Apple','ImageUrl':'/logo/realme.jpg'}];
     N.Page.Brand.init(_brands);
     // $(Ids.MobileRepair).on('mouseover',function(){
     //     console.log("hovered");
