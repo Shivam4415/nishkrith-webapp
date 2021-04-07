c9f719c1f98b68cbead329f5f12e83dcb3e87351
Shivam@RS <shivam441@gmail.com>
2021-04-07 07:25:18 +0530

    Space Formatting js code

--- javascript/nishkritih/page/n.page.color.js
+++ javascript/nishkritih/page/n.page.color.js
@@ -1,17 +1,17 @@
-N.Page.Color=new function(){
-var init=function(){
-    var _parentContainerId="#Color";
-        var _outerGrid='<div uk-grid name="grid"></div>';
-         $(_parentContainerId).append(_outerGrid);
-        var _grid=$(_parentContainerId).find('[ name="grid"]');
-        var _colors=arguments[0];
-        _colors.forEach(function(p){
-            var _cardHtml=N.Page.Util.getCard(p.Id,p.Name,p.ImageUrl);
-            _grid.append(_cardHtml);
-        });
-}
+N.Page.Color = new (function () {
+  var init = function () {
+    var _parentContainerId = "#Color";
+    var _outerGrid = '<div uk-grid name="grid"></div>';
+    $(_parentContainerId).append(_outerGrid);
+    var _grid = $(_parentContainerId).find('[ name="grid"]');
+    var _colors = arguments[0];
+    _colors.forEach(function (p) {
+      var _cardHtml = N.Page.Util.getCard(p.Id, p.Name, p.ImageUrl);
+      _grid.append(_cardHtml);
+    });
+  };
 
-    return {
-    init:init
-};
-};
+  return {
+    init: init,
+  };
+})();
