fd997fda1130379485a0ba177903b3ad9f22291e
Shivam@RS <shivam441@gmail.com>
2021-04-07 05:54:27 +0530

    Fixed code for login and signup modal on home page

--- javascript/nishkritih/page/n.page.home.js
+++ javascript/nishkritih/page/n.page.home.js
@@ -1,27 +1,46 @@
-N.Page.Home= new function(){
-   var Ids={
-      MobileRepair:"#mobileRepair"
-   };
-   var init=function(){
-      var _brands=[{Id:1, 'Name':'Apple','ImageUrl':'/logo/apple.jpg'},
-      {'Id':2, 'Name':'Apple','ImageUrl':'/logo/realme.jpg'}];
-      N.Page.Brand.init(_brands);
-      // $(Ids.MobileRepair).on('mouseover',function(){
-      //     console.log("hovered");
-      
-      // });
-      // $(Ids.MobileRepair).on('click',function(){
-      //     console.log("clicked");
-      
-      // });
-       
-   }
-  
-  return{
-      init:init
-  }
+N.Page.Home = new (function () {
+  const Ids = {
+    MobileRepair: "#mobileRepair",
+    HomePageButtonLogIn: "#btnHomePageLogIn",
+    HomePageButtonSignUp: "#btnHomePageSignUp",
+    LoginModal: "#loginModal",
+    RegisterModal: "#registerModal",
   };
-  
-  window.onload=function(){
-   N.Page.Home.init();
+  const init = () => {
+    const _brands = [
+      { Id: 1, Name: "Apple", ImageUrl: "/logo/apple.jpg" },
+      { Id: 2, Name: "Realme", ImageUrl: "/logo/realme.jpg" },
+    ];
+    N.Page.Brand.init(_brands);
+
+    // $(Ids.MobileRepair).on('mouseover',function(){
+    //     console.log("hovered");
+
+    // });
+    // $(Ids.MobileRepair).on('click',function(){
+    //     console.log("clicked");
+
+    // });
+    _initModal();
   };
+  const _initModal = function () {
+    $(Ids.HomePageButtonLogIn).on("click", openLoginModal);
+    $(Ids.HomePageButtonSignUp).on("click", openSignUpModal);
+  };
+
+  const openLoginModal = () => {
+    UIkit.modal(Ids.LoginModal).show();
+  };
+
+  const openSignUpModal = () => {
+    UIkit.modal(Ids.RegisterModal).show();
+  };
+
+  return {
+    init: init,
+  };
+})();
+
+window.onload = function () {
+  N.Page.Home.init();
+};
