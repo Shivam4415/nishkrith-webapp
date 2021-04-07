02da1101ce31f2ba67d89803fd80c53913799075
Shivam@RS <shivam441@gmail.com>
2021-04-07 06:48:36 +0530

    all image is shown by javascript

--- javascript/nishkritih/page/n.page.home.js
+++ javascript/nishkritih/page/n.page.home.js
@@ -3,16 +3,23 @@ N.Page.Home = new (function () {
     MobileRepair: "#mobileRepair",
     HomePageButtonLogIn: "#btnHomePageLogIn",
     HomePageButtonSignUp: "#btnHomePageSignUp",
-    LoginPageButtonSignUp:"#btnSignUp",
+    LoginPageButtonSignUp: "#btnSignUp",
     LoginModal: "#loginModal",
     RegisterModal: "#registerModal",
-
-
   };
   const init = () => {
     const _brands = [
       { Id: 1, Name: "Apple", ImageUrl: "/logo/apple.jpg" },
       { Id: 2, Name: "Realme", ImageUrl: "/logo/realme.jpg" },
+      { Id: 3, Name: "Asus", ImageUrl: "/logo/asus.jpg" },
+      { Id: 4, Name: "Mi", ImageUrl: "/logo/mi.jpg" },
+      { Id: 5, Name: "Motorola", ImageUrl: "/logo/motorola.jpg" },
+      { Id: 6, Name: "Honor", ImageUrl: "/logo/honor.jpg" },
+      { Id: 7, Name: "Nokia", ImageUrl: "/logo/nokia.jpg" },
+      { Id: 8, Name: "OnePlus", ImageUrl: "/logo/oneplus.jpg" },
+      { Id: 9, Name: "Samsung", ImageUrl: "/logo/samsung.jpg" },
+      { Id: 10, Name: "Sony", ImageUrl: "/logo/sony.jpg" },
+      { Id: 11, Name: "Vivo", ImageUrl: "/logo/vivo.jpg" },
     ];
     N.Page.Brand.init(_brands);
 
@@ -30,7 +37,6 @@ N.Page.Home = new (function () {
     $(Ids.HomePageButtonLogIn).on("click", openLoginModal);
     $(Ids.HomePageButtonSignUp).on("click", openSignUpModal);
     $(Ids.LoginPageButtonSignUp).on("click", openSignUpModal);
-
   };
 
   const openLoginModal = () => {
--- javascript/nishkritih/page/n.page.util.js
+++ javascript/nishkritih/page/n.page.util.js
@@ -1,25 +1,35 @@
-N.Page.Util=new function(){
-    var createCard=function(id,name,imgUrl,type){
-        if(type=='Varient'){
-            return '<a class="">'+
-                    '<img src="'+imgUrl+'" uk-cover class="">'+
-                    '<canvas width="600" height="400"></canvas>'+
-                   '</a>'+
-                   '<a class="uk-card-body">'+
-                //   '<h3 class="uk-card-title">'+name+'</h3>'+
-                   '<p></p>'+
-                   '</a>'
-        }
-       return '<a class="uk-padding-remove " name="'+id+'">'+
-            '<div class="uk-card uk-card-default uk-card-body uk-margin-left uk-card-small uk-width-small@s">'+
-                '<img src="'+imgUrl+'" class="">'+
-            '</div>'+
-            '<div class="uk-card-title">'+
-                // '<h3 class="uk-card-title">'+name+'</h3>'+
-            '</div>'+
-        '</a>';
-    };
-    return{
-        getCard:createCard
+N.Page.Util = new (function () {
+  var createCard = function (id, name, imgUrl, type) {
+    if (type == "Varient") {
+      return (
+        '<a class="">' +
+        '<img src="' +
+        imgUrl +
+        '" uk-cover class="">' +
+        '<canvas width="600" height="400"></canvas>' +
+        "</a>" +
+        '<a class="uk-card-body">' +
+        //   '<h3 class="uk-card-title">'+name+'</h3>'+
+        "<p></p>" +
+        "</a>"
+      );
     }
-};
+    return (
+      '<a class="uk-padding-remove " name="' +
+      id +
+      '">' +
+      '<div class="uk-card uk-card-default uk-card-body uk-margin-left uk-card-small uk-width-small@s">' +
+      '<img src="' +
+      imgUrl +
+      '" class="">' +
+      "</div>" +
+      '<div class="uk-card-title">' +
+      // '<h3 class="uk-card-title">'+name+'</h3>'+
+      "</div>" +
+      "</a>"
+    );
+  };
+  return {
+    getCard: createCard,
+  };
+})();
