a4bd99e909ad6f2d021478489c56cda432a2b0e8
Shivam@RS <shivam441@gmail.com>
2021-04-12 01:44:19 +0530

    added click event

--- javascript/nishkritih/page/n.page.brand.js
+++ javascript/nishkritih/page/n.page.brand.js
@@ -12,7 +12,7 @@ N.Page.Brand = new (function () {
     var _grid = $(_parentContainerId).find('[ name="grid"]');
     var _brands = arguments[0];
     _brands.forEach(function (b) {
-      var _cardHtml = N.Page.Util.getCard(b.Id, b.Name, b.ImageUrl);
+      var _cardHtml = N.Page.Util.getCard(b.id, b.name, b.logo);
       _grid.append(_cardHtml);
       $(_parentContainerId).on("click", '[name="' + b.Id + '"]', function () {
         $(_containerIds.Color).addClass("uk-hidden");
