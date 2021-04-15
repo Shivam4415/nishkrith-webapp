d2e6ea2e6771af3bc227a1d2ac96e24d81f876da
Shivam@RS <shivam441@gmail.com>
2021-04-15 16:09:30 +0530

    Fixed data mapping from server

--- javascript/nishkritih/page/n.page.color.js
+++ javascript/nishkritih/page/n.page.color.js
@@ -1,26 +1,24 @@
-N.Page.Color = new (function () {
-  var init = function () {
-    var _parentContainerId = "#Color";
-    var _outerGrid = '<div uk-grid name="grid"></div>';
-    var _logInOffCanvas="#offcanvas-flip";
-    $(_parentContainerId).append(_outerGrid);
-    var _grid = $(_parentContainerId).find('[ name="grid"]');
-    var _colors = arguments[0];
-    _colors.forEach(function (p) {
-      var i=0;
-      var _cardHtml = N.Page.Util.getCard(p[i].id, p[i].name, p[i].logo);
-      i++;
-      _grid.append(_cardHtml);
-      $(_parentContainerId).on('click','[name="'+p.id+'"]',function(){
-        //check if user is logged in 
-        //if not then show log in interface.
-       UIkit.offcanvas($(_logInOffCanvas)).show();
-       //N.Page.Home.openSignUpModal();
-    });
-    });
-  };
+N.Page.Color = new(function() {
+    var init = function() {
+        var _parentContainerId = "#Color";
+        var _outerGrid = '<div uk-grid name="grid"></div>';
+        var _logInOffCanvas = "#offcanvas-flip";
+        $(_parentContainerId).append(_outerGrid);
+        var _grid = $(_parentContainerId).find('[ name="grid"]');
+        var _colors = arguments[0];
+        _colors.forEach(function(p) {
+            var _cardHtml = N.Page.Util.getCard(p.id, p.name, p.logo);
+            _grid.append(_cardHtml);
+            $(_parentContainerId).on('click', '[name="' + p.id + '"]', function() {
+                //check if user is logged in 
+                //if not then show log in interface.
+                UIkit.offcanvas($(_logInOffCanvas)).show();
+                //N.Page.Home.openSignUpModal();
+            });
+        });
+    };
 
-  return {
-    init: init,
-  };
+    return {
+        init: init,
+    };
 })();
