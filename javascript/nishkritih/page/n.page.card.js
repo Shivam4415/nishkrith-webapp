705d10a7d3f0b7495f5262959c844b8fb44774df
Shivam@RS <shivam441@gmail.com>
2021-04-29 22:14:36 +0530

    Loading images

--- javascript/nishkritih/page/n.page.card.js
+++ javascript/nishkritih/page/n.page.card.js
@@ -1,6 +1,7 @@
 N.Page.Block = new (function () {
     var createCard = function (id, name, imgUrl, type) {
-   
+      $(`#`+id).parent().removeClass("uk-animate");
+
       return (
         document.getElementById(id).src=imgUrl
       );
